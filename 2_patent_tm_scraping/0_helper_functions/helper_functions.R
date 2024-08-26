d917c753e9428c5697bb4b960f2c0483478762b1
amags727 <86733530+amags727@users.noreply.github.com>
2024-08-26 19:08:05 -0400

    updated scraping code

--- 2_patent_tm_scraping/0_helper_functions/helper_functions.R
+++ 2_patent_tm_scraping/0_helper_functions/helper_functions.R
@@ -61,91 +61,81 @@ nodes_to_df <- function(nodes){
   return(result)
 }
 
-scraping_iteration = function(user_name, password, type){
-  siren_numbers = fread(file.path(type,'siren_numbers.csv'), colClasses = list(character = "siren"))
-  failed_attempts = data.frame(value = 0); fwrite(failed_attempts, file.path(type,'failed_attempts.csv')) 
-  
-  while  (failed_attempts$value[1] <3){
-    ## check to see if we have number lists / counters left over from last attempt
-    if (file.exists(file.path(type,'counter.RDS')) & file.exists(file.path(type,'number_list.RDS'))){
-      counter <- readRDS(file.path(type,'counter.RDS'))
-      number_list <- readRDS(file.path(type,'number_list.RDS'))
+scraping_siren_version = function(node_num, user_name, password, type){
+  siren_path = paste0('data/2_patent_tm_scraping/2_working/',type,'_siren_numbers_',node_num,'.csv')
+  counter_path = paste0('data/2_patent_tm_scraping/2_working/counter',node_num,'.RDS')
+  number_path = paste0('data/2_patent_tm_scraping/2_working/number_list',node_num,'.RDS')
+  scraping_path = 'IWH_code/2_patent_tm_scraping/0_helper_functions/scraping_script.sh'
+  siren_numbers = fread(siren_path, colClasses = list(character = "siren"))
+  failed_attempts = 0
+  while(failed_attempts < 3 & any(is.na(siren_numbers$category))){
+    # set up number list to check & counter for what sublist we're on 
+    if (file.exists(counter_path) & file.exists(number_path)){ # if we have number list from last attempt
+      counter <- readRDS(counter_path); number_list <- readRDS(number_path)
     }else{
-      siren_numbers_unsolved <- siren_numbers[is.na(category)]
-      number_list <- list(siren_numbers_unsolved$siren[1:min(nrow(siren_numbers_unsolved), 2000)])
+      number_list = siren_numbers[is.na(category)][['siren']] %>% .[1:min(nrow(.), 2000)] %>% list()
       counter <- 1
     }
-    output = data.frame(siren = character(), category = numeric())
-    while (counter <= length(number_list) & failed_attempts$value[1] <3){
-      print(nrow(siren_numbers %>% filter(!is.na(category))) / nrow(siren_numbers))
-      tryCatch({
-        current_numbers = number_list[[counter]]
-        if (type == "1_patent"){
-          arguments = paste0("(",paste(paste0('[DESI=',current_numbers,']'), collapse = ' OU '),") ET [DEPD=1990:2023]")
-        } else{
+    ## go through the sublists looking for patents 
+    output = data.frame(siren = character(), category = numeric()) 
+    while (counter <= length(number_list) & failed_attempts <3){
+      current_numbers = number_list[[counter]]
+      if (type == "patent"){
+        arguments = paste0("(",paste(paste0('[DESI=',current_numbers,']'), collapse = ' OU '),") ET [DEPD=1990:2023]")
+      }else{
         arguments = paste0("(",paste(paste0('[ApplicantIdentifier=',current_numbers,']'), collapse = ' OU '),") ET [ApplicationDate=1990:2023]")
+      }
+      system2(scraping_path, args = c(user_name, password,type,shQuote(arguments),node_num))
+      scraping_result = tryCatch({
+        scraping_result = read_xml(paste0('data/2_patent_tm_scraping/2_working/scraping_output', node_num,'.xml'))
+        if(length(xml_find_all(scraping_result, "//metadata")) != 1){scraping_result = 1}; scraping_result = scraping_result
+      }, error = function(e){print('non-readable'); scraping_result = 1})
+      
+      if(typeof(scraping_result) == "double"){failed_attempts = failed_attempts + 1; print(paste0('failed ', failed_attempts))
+      }else{ # we successfully scraped
+        if (length(scraping_result)==0){  #we know there are no firms with patents in this list 
+          output = rbind(output,data.frame(siren = current_numbers, category = 0))
+        }else if (length(current_numbers)!=1){  # we know there are firms with patents in this list (but not which one)
+          midway_point = floor(length(current_numbers) / 2)
+          number_list[[length(number_list)+1]] = current_numbers[1:midway_point]
+          number_list[[length(number_list)+1]] = current_numbers[(midway_point+1): length(current_numbers)]
+        }else if (length(xml_data)!=10000){  # we have identified a patenting firm and know all of its patents  
+          paste0('data/2_patent_tm_scraping/2_working/', type,"_siren/results", current_numbers[1], '.csv') %>%
+            fwrite(nodes_to_df(xml_data),.)
+          output = rbind(output,data.frame(siren = current_numbers, category = 1))
+        }else{  #we have identified a patenting firm, and need to split its patents up by time period 
+          output = rbind(output,data.frame(siren = current_numbers, category = 2))
         }
-        system2('0_input_data/scraping_script.sh', args =  c(user_name, password,type,shQuote(arguments)))
-        xml_data = read_xml(paste0(type,'_scraping_output.xml'))
-        
-        ## if we have succeeded at doing the search 
-        if (length(xml_find_all(xml_data, "//metadata")) == 1){
-          xml_data <- read_xml(paste0(type,'_scraping_output.xml')) %>% xml_find_all( "//result")
-          #we know there are no firms with patents in this list 
-          if(length(xml_data) == 0){
-            output = rbind(output,data.frame(siren = current_numbers, category = 0))
-            
-            # we know there are firms with patents in this list (but not which one)
-          }else if (length(current_numbers)!=1){ 
-            midway_point = floor(length(current_numbers) / 2)
-            number_list[[length(number_list)+1]] = current_numbers[1:midway_point]
-            number_list[[length(number_list)+1]] = current_numbers[(midway_point+1): length(current_numbers)]
-            
-            # we have identified a patenting firm and know all of its patents  
-          } else if (length(xml_data)!=10000){
-            fwrite(nodes_to_df(xml_data), paste0(type,'/results_siren/results_', current_numbers[1], '.csv'))
-            output = rbind(output,data.frame(siren = current_numbers, category = 1))
-            
-            # we have identified a patenting firm, and need to split its patents up by time period 
-          } else{
-            output = rbind(output,data.frame(siren = current_numbers, category = 2))
-          }
-          counter = counter+1; reset_failed_attempts(type)
-          
-          # if we didn't do the search or hit another error mark it down  
-        }else{add_failed_attempt(type)}}, error = function(e){ add_failed_attempt(type)})
-      failed_attempts = fread(file.path(type,'failed_attempts.csv'))
-      file.remove(paste0(type,'_scraping_output.xml'))
+        counter = counter+1; failed_attempts = 0
+      }
     }
-    ## if we exited bc we hit 3 failed attempts, save the counter and number_list
-    if (failed_attempts$value[1] == 3){
-      saveRDS(counter, file.path(type,'counter.RDS')); saveRDS(number_list, file.path(type,'number_list.RDS'))
-    }else{
-      file.remove(file.path(type,'counter.RDS')); file.remove(file.path(type,'number_list.RDS'))
-      print(paste0('took ', counter -1, ' iterations'))
-    }
+    ## if we exited bc we hit 3 failed attempts, save the counter and number_list; otherwise remove
+    if (failed_attempts == 3){saveRDS(counter, counter_path); saveRDS(number_list, number_path)
+    }else{file.remove(counter_path); file.remove(number_path)}
     siren_numbers = merge(siren_numbers, output, by = 'siren', all.x = T)
     siren_numbers = siren_numbers[,category := ifelse(is.na(category.x), category.y, category.x)] %>% select(-c(category.x, category.y))
-    fwrite(siren_numbers, file.path(type,'siren_numbers.csv'))
-    print(nrow(siren_numbers %>% filter(!is.na(category))) / nrow(siren_numbers))
+    fwrite(siren_numbers, siren_path)
   }
-  }
+}
 
-scraping_iteration_dates_only = function(node_num, user_name, password, type){
+
+scraping_date_version = function(node_num, user_name, password, type){
   failed_attempts = 0
   date_path = paste0('data/2_patent_tm_scraping/2_working/',type,'_dates_',node_num,'.csv')
   dates = fread(date_path) %>% mutate(across(-completed, ~as.Date(., format = "%Y%m%d")))
   
   while (failed_attempts < 3 & min(dates$completed) == 0){
-    i = which(dates$completed == 0)[1]; date_string = paste0(dates$start_dates[i], ":",dates$end_dates[i]) %>% gsub("-", "", .)
+    i = which(dates$completed == 0)[1]
+    date_string = paste0(dates$start_dates[i], ":",dates$end_dates[i]) %>% gsub("-", "", .)
     if (type =="patent"){date_string =  paste0('[DEPD=', date_string,']')}else{date_string =  paste0('[ApplicationDate=', date_string,']')}
     system2('IWH_code/2_patent_tm_scraping/0_helper_functions/scraping_script.sh', args =  c(user_name, password,type,date_string, node_num))
     
     output = tryCatch({
-      output = read_xml(paste0('data/2_patent_tm_scraping/2_working/scraping_output', node_num,'.xml'))}, 
-      error = function(e){output = 1})
+      output = read_xml(paste0('data/2_patent_tm_scraping/2_working/scraping_output', node_num,'.xml'))
+      if(length(xml_find_all(output, "//metadata")) != 1){output = 1}; output = output
+      }, error = function(e){print('non-readable'); output = 1})
     
-    if(typeof(output) == "double"){failed_attempts = failed_attempts + 1; print('failed')}else{
+    if(typeof(output) == "double"){failed_attempts = failed_attempts + 1; print(paste0('failed ', failed_attempts))}else{
       failed_attempts = 0
       nodes <- xml_find_all(output, "//result")
       
--- 2_patent_tm_scraping/1_scrape_patents.R
+++ 2_patent_tm_scraping/1_scrape_patents.R
@@ -18,20 +18,26 @@ helper_functions = ls()
 #                .[,category := numeric()]
 # fwrite(siren_numbers, 'data/2_patent_tm_scraping/2_working/patent_siren_numbers.csv')
 # fwrite(siren_numbers, 'data/2_patent_tm_scraping/2_working/tm_siren_numbers.csv')
+#
 # 
-# 
-# ##import date file we need to iterate through
+# # ##import date file we need to iterate through
 # dates = data.frame(start_dates = seq(ymd("1990-01-01"), ymd("2023-01-01"), by="1 weeks"),
 #                    end_dates = seq(ymd("1990-01-07"), ymd("2023-01-07"), by="1 weeks"),
 #                    completed = 0)
 # 
 # fwrite(dates,'data/2_patent_tm_scraping/2_working/patent_dates.csv')
 # fwrite(dates,'data/2_patent_tm_scraping/2_working/tm_dates.csv')
-# 
 
 
-# 1 scrape all patent data from the time period ---------------------------------------------------------
-type = 'patent'; num_cores = 1
+DENE_list = lapply(dir('data/2_patent_tm_scraping/2_working/patent_time'), function(stub){
+            fread(paste0('data/2_patent_tm_scraping/2_working/patent_time/', stub)) %>% select(DENE)
+            }) %>% rbindlist() %>% unique() %>% as.data.table() %>% .[, DENE := str_cleaning(DENE)] %>% 
+            separate_rows(DENE, sep = ",") %>% as.data.table() %>% .[, DENE := str_trim(DENE)] %>% unique() 
+
+
+
+# 1 scrape all patent data using application date periods ---------------------------------------------------------
+type = 'patent'; num_cores = 8
 
 
 ## SET UP THE INPUTS FOR EACH NODE 
@@ -39,7 +45,7 @@ dirlist = dir('data/2_patent_tm_scraping/2_working/') %>% .[grepl(paste0(type,"_
 if (length(dirlist) ==0){
   dates = fread(paste0('data/2_patent_tm_scraping/2_working/',type,'_dates.csv'))
 }else{
-  dates = lapply(dir('data/2_patent_tm_scraping/2_working/') %>% .[grepl(paste0(type,"_dates_"),.)],function(stub){
+  dates = lapply(dirlist,function(stub){
           file_name = paste0('data/2_patent_tm_scraping/2_working/',stub)    
           output = fread(file_name); file.remove(file_name); return(output)}) %>% rbindlist()
   fwrite(dates, paste0('data/2_patent_tm_scraping/2_working/',type,'_dates.csv'))
@@ -59,140 +65,72 @@ parLapply(cl,1:num_cores, function(node_num){
     if(is.na(login_index)){
       login_info$availability = 0
     }else{
+      print(paste0("starting: ",login_index))
       user_name = login_info$user_name[login_index];
       password = login_info$password[login_index]
       login_info$availability[login_index] = 1
-      scraping_iteration_dates_only(node_num, user_name, password, type)
+      scraping_date_version(node_num, user_name, password, type)
     }
   }
 })
 stopCluster(cl)
     
     
-  
 
-# Scrape all the trademark data from the time period-------------------------------------------
-type = '2_trademark'
-keep_going = fread(file.path(type, 'dates.csv')) %>% pull(completed) %>% min() == 0
-while (keep_going){
-  login_info = fread('0_input_data/login_info.csv')
-  i = which(login_info$availability == 0)[1]
-  
-  if(is.na(i)){ # if no available nodes just reset those on cool-down and try again. 
-    login_info$availability = 0
-    fwrite(login_info,'0_input_data/login_info.csv')
-  } else{
-    login_info$availability[i] = 1
-    fwrite(login_info,'0_input_data/login_info.csv')
-    scraping_iteration_dates_only(login_info$user_name[i],
-                                  login_info$password[i], type)
-    print(paste('login num:', i))
-  }
-  keep_going = fread(file.path(type, 'dates.csv')) %>% pull(completed) %>% min() == 0
+
+
+
+# 2 scrape all patent data using siren codes  -----------------------------
+type = 'patent'; num_cores = 8
+
+## SET UP THE INPUTS FOR EACH NODE 
+dirlist = dir('data/2_patent_tm_scraping/2_working/') %>% .[grepl(paste0(type,"_siren_numbers_"),.)] 
+if (length(dirlist) ==0){
+  siren_numbers = fread(paste0('data/2_patent_tm_scraping/2_working/',type,'_siren_numbers.csv'),
+                  colClasses = list(character = "siren"))
+}else{
+  siren_numbers = lapply(dirlist,function(stub){
+                  file_name = paste0('data/2_patent_tm_scraping/2_working/',stub)    
+                  output = fread(file_name, colClasses = list(character = "siren"))
+                  file.remove(file_name); return(output)}) %>% rbindlist()
+  fwrite(siren_numbers, paste0('data/2_patent_tm_scraping/2_working/',type,'_siren_numbers.csv'))
 }
+siren_numbers = siren_numbers %>% split(., ceiling(seq_len(nrow(.)) / ceiling(nrow(.) / num_cores)))
+for (i in 1:num_cores){fwrite(siren_numbers[[i]], paste0('data/2_patent_tm_scraping/2_working/',type,'_siren_numbers_',i,'.csv'))}
 
 
+### RUN THE SCRAPING CODE IN PARALLEL
+cl <- makeCluster(num_cores); clusterExport(cl,c(helper_functions,"type"));
+clusterEvalQ(cl, {lapply(packages, library, character.only = T)})
 
+parLapply(cl,1:num_cores, function(node_num){
+  # node_num = 1
+  siren_path = paste0('data/2_patent_tm_scraping/2_working/',type,'_siren_numbers_',node_num,'.csv')
+  login_path = 'IWH_code/2_patent_tm_scraping/0_helper_functions/login_info.csv'
 
-# 4 go through all siren codes searching for patents  ---------------------------------------------------------
-type = '1_patent'
-keep_going = any(is.na(fread(file.path(type,'siren_numbers.csv'))[['category']]))
-
-while (keep_going){
-  login_info = fread('0_input_data/login_info.csv')
-  chosen_index = which(login_info$availability == 0)[1]
-  
-  if(is.na(chosen_index)){ # if no available nodes just reset those on cool-down and try again. 
-    login_info$availability = 0
-    fwrite(login_info,'0_input_data/login_info.csv')
-  } else{
-    scraping_iteration(login_info$user_name[chosen_index],
-                       login_info$password[chosen_index], type)
-    login_info$availability[chosen_index] = 1
-    fwrite(login_info,'0_input_data/login_info.csv')
-    print(paste('login num:', chosen_index))
+  while(fread(siren_path) %>% pull(category) %>% any(is.na(.))){
+    login_info = fread(login_path); login_index = which(login_info$availability == 0)[1]
+    if(is.na(login_index)){
+      login_info %>% mutate(availability = 0) %>% fwrite(., login_path)
+    }else{
+      login_info$availability[login_index] = 1; fwrite(login_info, login_path);
+      print(paste0("starting: ",login_index))
+      user_name = login_info$user_name[login_index];
+      password = login_info$password[login_index]
+      scraping_siren_version(node_num, user_name, password, type)
+    }  
   }
-  keep_going = any(is.na(fread(file.path(type,'siren_numbers.csv'))[['category']]))
-}
-
-# 5 go through all siren codes searching for trademarks  ---------------------------------------------------------
-type = '2_trademark'
-keep_going = any(is.na(fread(file.path(type,'siren_numbers.csv'))[['category']]))
-
-while (keep_going){
-  login_info = fread('0_input_data/login_info.csv')
-  chosen_index = which(login_info$availability == 0)[1]
-  
-  if(is.na(chosen_index)){ # if no available nodes just reset those on cool-down and try again. 
-    login_info$availability = 0
-    fwrite(login_info,'0_input_data/login_info.csv')
-  } else{
-    login_info$availability[chosen_index] = 1
-    fwrite(login_info,'0_input_data/login_info.csv')
-    scraping_iteration(login_info$user_name[chosen_index],
-                       login_info$password[chosen_index], type)
-    print(paste('login num:', chosen_index))
-  }
-  keep_going = any(is.na(fread(file.path(type,'siren_numbers.csv'))[['category']]))
-}
-
-# 4 combine scraped data into unique files  -------------------------------------
-file_list = dir('1_patent/results_time')
-
-patent_from_dates_raw = lapply(file_list, function(file){
-  file = fread(file.path('1_patent/results_time', file))
-  file = file %>% mutate(country = substr(file$PUBN,10,11),
-                         across(c(DEPN, PUBN),  ~(sub(".*<doc-number>(\\d+)</doc-number>.*", "\\1", .)))) %>%
-    select(country,everything())
-}) %>% rbindlist(use.names = T, fill = T) %>% unique()
-fwrite(patent_from_dates_raw, '1_patent/patents_from_time.csv')
-
-
-# 6 compile all the results from patent search --------------------------------------------------------
-file_list = dir('1_patent/results_siren')
-patents_siren = lapply(file_list, function(file){
-  siren_code = substr(file,9, 17)
-  file = fread(file.path('1_patent/results_siren', file))
-  file = file %>% mutate(siren = siren_code,
-                         country = substr(file$PUBN,10,11),
-                         across(c(DEPN, PUBN),  ~(sub(".*<doc-number>(\\d+)</doc-number>.*", "\\1", .)))) %>%
-    select(siren, country,everything())
-}) %>% rbindlist(use.names = T, fill = T)
-fwrite(patent_from_siren_raw, '1_patent/patents_from_siren.csv')
+})
+stopCluster(cl)
 
 
 
-patents_siren = fread('1_patent/patents_from_siren.csv')
-patents_siren_unnested  = patents_siren %>% rename(DENE_list = DENE) 
-
-patents_siren_unnested = patents_siren_unnested[, DENE := str_split(str_trim(DENE_list), "\\s*,\\s*")]
-patents_siren_unnested = patents_siren_unnested[, DENE_list_length := lengths(DENE)] %>% unnest(DENE) %>% as.data.table() 
-patents_siren_unnested[, DENE :=  str_cleaning(DENE)]
-
-### MAKE SURE TO HANDLE THE POSSIBILITY OF DUPLICATE OBSERVATIONS (multiple sirens attached to the same patent)
 
 
-## use the official siren-name list as 1st most reliable  
-siren_DENE_official =fread('0_input_data/StockUniteLegaleHistorique_utf8 2.csv',
-                           select = c('denominationUniteLegale', 'siren'),  colClasses = list(character = "siren")) %>%
-  rename(DENE = denominationUniteLegale)
-siren_DENE_official[, DENE := str_cleaning(DENE)]
-siren_DENE_official = siren_DENE_official[!duplicated(DENE) & DENE!= ""] 
 
 
-## in cases where our siren code search returned an entry with one DENE entry;
-## assign the applicant to that siren code as 2nd most reliable 
-siren_DENE_unique_ids = patents_siren_unnested[DENE_list_length == 1] %>% 
-  select(siren, DENE) %>% unique() %>% arrange(siren, DENE)
 
-## now identify which DENE entry was most commonly associated with a given siren code 
-## as 3rd most reliable
-siren_DENE_dominant_ids <- patents_siren_unnested[, .N, by = .(siren, DENE)][order(-N), .SD[1], by = siren] 
 
-### IF A DENE remains unmatched after all these processes
-### assign it to the siren code it was found with as fourth most reliable 
-
-### Finally, use the most similiar matched siren code as our last check
 
 
 
